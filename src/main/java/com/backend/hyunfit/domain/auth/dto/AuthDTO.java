e3806530fca917028cfcee3f00a0fc9c3e704c97
Daniel Choi <schoiak@connect.ust.hk>
2023-08-30 10:55:56 +0900

    feat: Admin 인증 기능 추가

--- src/main/java/com/backend/hyunfit/domain/auth/dto/AuthDTO.java
+++ src/main/java/com/backend/hyunfit/domain/auth/dto/AuthDTO.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class AuthDTO {
-    String mbrId;
-    String mbrPw;
+    String username;
+    String password;
     boolean authenticated;
 }
--- src/main/java/com/backend/hyunfit/domain/auth/service/AuthService.java
+++ src/main/java/com/backend/hyunfit/domain/auth/service/AuthService.java
@@ -1,7 +1,19 @@
 package com.backend.hyunfit.domain.auth.service;
 
 import com.backend.hyunfit.domain.auth.dto.AuthDTO;
+import com.backend.hyunfit.global.exception.BusinessException;
 
 public interface AuthService {
+
+    /**
+     * 사용자 인증 정보를 생성하는 메서드입니다.
+     *
+     * @param authDTO 인증 정보 DTO 객체입니다.
+     * @return 입력된 인증 정보 DTO 객체를 반환합니다.
+     * @throws BusinessException 입력된 사용자 이름에 해당하는 회원 정보가 없거나 비밀번호가 일치하지 않을 경우 발생하는 예외입니다.
+     *                          이 예외는 ErrorCode.MEMBER_NOT_FOUND 에러 코드와 함께 발생합니다.
+     */
     AuthDTO createMemberAuth(AuthDTO authDTO);
+    AuthDTO createAdminAuth(AuthDTO authDTO);
+    AuthDTO createTrainerAuth(AuthDTO authDTO);
 }
