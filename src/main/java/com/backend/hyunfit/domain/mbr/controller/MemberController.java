0a4b2b10f04c47f8eebbe2424955973d0892d3ff
Seung-ryeol Choi <100273844+danielchoi1115@users.noreply.github.com>
2023-09-03 17:52:31 +0900

    Chore: 사용하지 않는 dependency 제거
    org.apache.coyote.Response는 사용하는 곳이 없어 제거

--- src/main/java/com/backend/hyunfit/domain/mbr/controller/MemberController.java
+++ src/main/java/com/backend/hyunfit/domain/mbr/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.backend.hyunfit.domain.mbr.dto.MemberDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
