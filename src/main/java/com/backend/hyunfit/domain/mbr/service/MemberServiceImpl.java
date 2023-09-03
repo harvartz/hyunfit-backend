2c6fa08903bfb58891c23d0237c00a0b47976b17
Seung-ryeol Choi <100273844+danielchoi1115@users.noreply.github.com>
2023-09-03 17:54:16 +0900

    Chore: 미사용 dependency 제거
    아래 import 제거
    import java.util.logging.Logger;
    import lombok.extern.java.Log;
    import java.util.List;

--- src/main/java/com/backend/hyunfit/domain/mbr/service/MemberServiceImpl.java
+++ src/main/java/com/backend/hyunfit/domain/mbr/service/MemberServiceImpl.java
@@ -14,16 +14,13 @@ import com.backend.hyunfit.global.dto.SearchDTO;
 import com.backend.hyunfit.global.exception.BusinessException;
 import com.backend.hyunfit.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 @Log4j2
 @Service
