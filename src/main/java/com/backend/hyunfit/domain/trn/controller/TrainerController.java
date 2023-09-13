20d6fe6833ceed64f366a72b3672adfd1d84ae87
YIJIHO <yjh9099@gmail.com>
2023-09-13 14:43:29 +0900

    Modify : BO-Trn 예약 관리 수정
    Resolve: #75

--- src/main/java/com/backend/hyunfit/domain/trn/controller/TrainerController.java
+++ src/main/java/com/backend/hyunfit/domain/trn/controller/TrainerController.java
@@ -1,5 +1,6 @@
 package com.backend.hyunfit.domain.trn.controller;
 
+import com.backend.hyunfit.domain.pt.dto.PersonalTrainingDTO;
 import com.backend.hyunfit.domain.trn.dto.TrainerDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -7,6 +8,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 
 @Tag(name="Trainer Controller", description = "트레이너 백오피스관련 컨트롤러")
@@ -18,7 +21,7 @@ public interface TrainerController {
             @ApiResponse(responseCode = "404", description = "NOT FOUND"),
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
     })
-    ResponseEntity<TrainerDTO> findAllPtBytrnSeq(String trnSeq);
+    ResponseEntity<List<PersonalTrainingDTO>> findAllPtBytrnSeq(String trnSeq);
 
     @Operation(summary = "트레이너의 작성되지않은 피드백 전체조회", description = "trnSeq를 확인 후 해당 trainer의 피드백 중 작성되지않은 피드백을 반환합니다.")
     @ApiResponses({
