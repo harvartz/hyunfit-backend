a038e2aa406b7cfbc72f33b4d6256327490b98c9
Daniel Choi <schoiak@connect.ust.hk>
2023-09-18 10:32:37 +0900

    add: add swagger https config

--- src/main/java/com/backend/hyunfit/HyunfitBackendApplication.java
+++ src/main/java/com/backend/hyunfit/HyunfitBackendApplication.java
@@ -1,9 +1,12 @@
 package com.backend.hyunfit;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "${backend-hyunfit-url}", description = "Default Server URL")})
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class HyunfitBackendApplication {
 
