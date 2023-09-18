250fca978933dc03da9122124201e996ca08bdcd
Daniel Choi <schoiak@connect.ust.hk>
2023-09-18 10:30:13 +0900

    Revert "add: add swagger https config"
    This reverts commit 9151c66fa502bf12244bbed7ad4bfc65837b7c90.

--- src/main/java/com/backend/hyunfit/HyunfitBackendApplication.java
+++ src/main/java/com/backend/hyunfit/HyunfitBackendApplication.java
@@ -1,12 +1,9 @@
 package com.backend.hyunfit;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class HyunfitBackendApplication {
 
